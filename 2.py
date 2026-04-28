28585795f67e40c62aef14dcd43e8ac6aeaf3290
trinabhkps <trinabhkps@gmail.com>
2026-04-28 08:57:45 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,9 +5,9 @@ st.title("My First Streamlit App")
 st.write("Hello! Creating a simple web application using streamlit library.")
 
 #Text input
-name = st.text_input("Enter your name:")
+name=st.text_input("Enter your name:")
 #Number input
-age = st.number_input("Enter your age:")
+age=st.number_input("Enter your age:")
 
 #Display a message when button is clicked
 if st.button("Submit"):
